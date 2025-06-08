89e7b43ea421b5871b95571b7c4a8a39f1cec0f9
simplyhemant <hs2312hemant@gmail.com>
2025-06-08 21:42:54 +0530

    started simplyCinema

---
+++ src/test/java/com/simply/Cinema/SimplyCinemaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simply.Cinema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplyCinemaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
