1db182fa0b65e04b1a24ac93d3f12788f8ed9029
simplyhemant <hs2312hemant@gmail.com>
2025-06-11 18:28:59 +0530

    added Auditing . Hehe

--- src/main/java/com/simply/Cinema/core/user/entity/User.java
+++ src/main/java/com/simply/Cinema/core/user/entity/User.java
@@ -1,10 +1,63 @@
 package com.simply.Cinema.core.user.entity;
 
-import jakarta.persistence.Entity;
+import com.simply.Cinema.core.systemConfig.entity.AuditTableEntity;
+import jakarta.persistence.*;
+import org.hibernate.envers.Audited;
 
 @Entity
-public class User {
+@Table(name = "app_user")
+@Audited
+public class User extends AuditTableEntity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
+    private String email;
 
+    private String passwordHash;
+
+    private String firstName;
+
+    private String lastName;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
--- src/main/java/com/simply/Cinema/core/user/repository/UserRepo.java
+++ src/main/java/com/simply/Cinema/core/user/repository/UserRepo.java
@@ -1,5 +1,10 @@
 package com.simply.Cinema.core.user.repository;
 
-public class UserRepo {
+import com.simply.Cinema.core.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepo extends JpaRepository<User, Long> {
 
 }
